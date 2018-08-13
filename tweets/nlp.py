c0494f59de22fdd2f623e785853fd4dcbb80344b
alexcrist <alexecrist@gmail.com>
2018-08-12 20:33:48 -0400

    Get tweet generation working

--- tweets/nlp.py
+++ tweets/nlp.py
@@ -1,61 +1,131 @@
-from nltk.corpus import genesis
-from nltk.collocations import BigramAssocMeasures, TrigramAssocMeasures, BigramCollocationFinder
 import re
 import csv
+import time
+from random import choice
+from nltk.data import load
+from collections import defaultdict
+
+TWEET_FILE = 'data/tweets.csv'
+START_TOKEN = '<START>'
+END_TOKEN = '<END>'
 
 def sanitize_tweet_word(word):
-  if word[0] is '\'':
-    word = word[1:]
-  if word[len(word) - 1] is '\'':
-    word = word[:-1]
+  # Remove leading and trailing apostrophes
+  word = word[1:] if (word[0] is '\'') else word
+  if len(word) == 0:
+    return word
+  word = word[:-1] if (word[len(word) - 1] is '\'') else word
+  
+  # Lowercase if not a mention (@) or a hashtag (#)
   special_word = (word[0] is '#') or (word[0] is '@')
-  return word if special_word else word.lower()
+  word = word if (special_word or word.isupper()) else word.lower()
+
+  return word
 
 def sanitize_tweet(tweet):
-  # Remove URLs
-  tweet = re.sub(r'http\S+', '', tweet)
-  # Purge punctuation
-  tweet = re.sub(r'[^\w\s\'#@_\?!]', '', tweet)
-  # Separate question and exclamation marks
-  tweet = re.sub(r'!', ' !', tweet)
-  tweet = re.sub(r'\?', ' ?', tweet)
-  # Split into array
-  tweet = tweet.split()
-  # Sanitize tweet words
-  tweet = list(map(sanitize_tweet_word, tweet))
-  # Remove retweet preface
-  if tweet[0] == 'rt':
+  tweet = re.sub(r'http\S+', '', tweet)           # Remove URLs
+  tweet = re.sub(r'[^\w\s\'#@_\?!\.]', '', tweet) # Purge punctuation
+  tweet = re.sub(r'!', ' !', tweet)               # Separate question marks 
+  tweet = re.sub(r'\?', ' ?', tweet)              # Separate exclamation marks
+  tweet = re.sub(r'\?', ' .', tweet)              # Separate periods
+  tweet = tweet.split()                           # Split into array
+  tweet = list(map(sanitize_tweet_word, tweet))   # Sanitize individual words
+  if (len(tweet) > 0) and (tweet[0] == 'RT'):     # Remove retweet stuff
     tweet = tweet[2:]
-  # Add starting and ending words
-  tweet.insert(0, '<start>')
-  tweet.append('<end>')
+
+  # Add starting and ending tokens
+  tweet.insert(0, START_TOKEN)
+  tweet.append(END_TOKEN)
   return tweet
 
-def read_data(filename):
-  with open(filename, 'r', encoding='utf-8') as csvfile:
+def read_tweets_from_file(file):
+  with open(file, 'r', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
-    count = 0
+    tweets = []
     for row in reader:
-      count += 1
-      if count is 50:
-        break
       tweet = row['text']
-      print()
-      print(tweet)
-      print(sanitize_tweet(tweet))
+      tweets += sanitize_tweet(tweet)
+  return tweets
 
-# '''def train_model(tweet):
-  
+# Builds a flat n-gram
+# i.e.: [('this', 'was'), ('was', 'a'), ('a', 'triumph')]
+def build_flat_ngram(string, n):
+  strings = [string[i:] for i in range(n)]
+  return zip(*strings)
 
-# '
-# words = genesis.words('english-web.txt')
+# Builds a bigram using a nested dictionary structure with weighted values
+# i.e.: {'this': {'was': 1}, 'was': {'a': 1}, 'a': {'triumph': 1}}
+def build_bigram(string):
+  flat_bigram = build_flat_ngram(string, 2)
+  bigram = defaultdict(lambda: defaultdict(int))
+  for word1, word2 in flat_bigram:
+    bigram[word1][word2] += 1
+  return bigram
 
-# bigram_measures = BigramAssocMeasures()
-# trigram_measures = TrigramAssocMeasures()
+# Builds a trigram using a nested dictionary structure with weighted values
+# i.e.: {'this': {'was': {'a': 1}}, 'was': {'a': {'triumph': 1}}}
+def build_trigram(string):
+  flat_trigram = build_flat_ngram(string, 3)
+  ngram = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+  for word1, word2, word3 in flat_trigram:
+    ngram[word1][word2][word3] += 1
+  return ngram
 
-# finder = BigramCollocationFinder.from_words(words)
+# Generates a tweet using the given bigram and trigram
+def generate_tweet(bigram, trigram):
+  tweet = ''
+  word1 = ''
+  word2 = START_TOKEN
 
-# results = finder.nbest(bigram_measures.pmi, 10)
-# print(results)'''
+  # Keeping adding words until we reach an end token
+  while word2 != END_TOKEN:
+    
+    # First try to use the trigram
+    choices = trigram[word1][word2]
 
-read_data('data/tweets.csv')
+    # Fallback on bigram if necessary
+    if len(choices.items()) == 0:
+      choices = bigram[word2]
+    
+    # Choose a new word based on the weighted values of the choices
+    flat_choices = []
+    for key, value in choices.items():
+      flat_choices += [key] * value
+    word3 = choice(flat_choices)
+    tweet += word3 + ' '
+    
+    # Advance generator words
+    word1 = word2
+    word2 = word3
+
+  # Reformat tweet
+  tweet = tweet[:-(len(END_TOKEN) + 2)] # Remove end token
+  tweet = re.sub(r' !', '!', tweet)     # Join question marks 
+  tweet = re.sub(r' \?', '?', tweet)    # Join exclamation marks
+  tweet = re.sub(r' \.', '.', tweet)    # Join periods marks
+
+  # Capitalize sentences
+  sentence_tokenizer = load('tokenizers/punkt/english.pickle')
+  sentences = sentence_tokenizer.tokenize(tweet)
+  sentences = [(sentence[0].upper() + sentence[1:]) for sentence in sentences]
+  tweet = ' '.join(sentences)
+
+  # Validate tweet
+  is_valid_tweet = len(tweet) <= 280
+  if is_valid_tweet:
+    return tweet
+  else:
+    return generate_tweet(bigram, trigram)
+
+if __name__ == '__main__':
+  print('Loading tweets...')
+  tweets = read_tweets_from_file(TWEET_FILE)
+  print('Building bigram...')
+  bigram = build_bigram(tweets)
+  print('Building trigram...')
+  trigram = build_trigram(tweets)
+  print('Tweet generator ready.')
+  while True:
+    input('Press [Enter] to generate.')
+    tweet = generate_tweet(bigram, trigram)
+    print(tweet)
